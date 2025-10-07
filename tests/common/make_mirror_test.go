68775a39caa4d9881c2530676951e17c84ab25e8
ronaldngounou <ronald.ngounou@yahoo.com>
2025-10-07 08:02:10 -0700

    Other functions in common/make_mirror_test
    Signed-off-by: ronaldngounou <ronald.ngounou@yahoo.com>

--- tests/common/make_mirror_test.go
+++ tests/common/make_mirror_test.go
@@ -22,7 +22,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"go.etcd.io/etcd/tests/v3/framework/config"
-	"go.etcd.io/etcd/tests/v3/framework/e2e"
 	"go.etcd.io/etcd/tests/v3/framework/testutils"
 )
 
@@ -46,7 +45,66 @@ func TestMakeMirrorModifyDestPrefix(t *testing.T) {
 	}
 }
 
-func testMirror(t *testing.T, srcTC, destTC testCase, mmOpts config.MakeMirrorOptions, sourcekvs, destkvs []testutils.KV, srcprefix, desprefix string) {
+func TestMakeMirrorNoDestPrefix(t *testing.T) {
+	testRunner.BeforeTest(t)
+	for _, srcTC := range clusterTestCases() {
+		for _, destTC := range clusterTestCases() {
+
+			t.Run(fmt.Sprintf("Source=%s/Destination=%s", srcTC.name, destTC.name), func(t *testing.T) {
+				var (
+					mmOpts     = config.MakeMirrorOptions{Prefix: "o_", NoDestPrefix: true}
+					sourcekvs  = []testutils.KV{{Key: "o_key1", Val: "val1"}, {Key: "o_key2", Val: "val2"}, {Key: "o_key3", Val: "val3"}}
+					destkvs    = []testutils.KV{{Key: "key1", Val: "val1"}, {Key: "key2", Val: "val2"}, {Key: "key3", Val: "val3"}}
+					srcprefix  = "o_"
+					destprefix = "key"
+				)
+
+				testMirror(t, srcTC, destTC, mmOpts, sourcekvs, destkvs, srcprefix, destprefix)
+			})
+		}
+	}
+}
+
+func TestMakeMirror(t *testing.T) {
+	testRunner.BeforeTest(t)
+	for _, srcTC := range clusterTestCases() {
+		for _, destTC := range clusterTestCases() {
+			t.Run(fmt.Sprintf("Source=%s/Destination=%s", srcTC.name, destTC.name), func(t *testing.T) {
+				var (
+					mmOpts    config.MakeMirrorOptions
+					sourcekvs = []testutils.KV{{Key: "key1", Val: "val1"}, {Key: "key2", Val: "val2"}, {Key: "key3", Val: "val3"}}
+					destkvs   = []testutils.KV{{Key: "key1", Val: "val1"}, {Key: "key2", Val: "val2"}, {Key: "key3", Val: "val3"}}
+					prefix    = "key"
+				)
+
+				testMirror(t, srcTC, destTC, mmOpts, sourcekvs, destkvs, prefix, prefix)
+
+			})
+		}
+	}
+}
+
+func TestMakeMirrorWithWatchRev(t *testing.T) {
+	testRunner.BeforeTest(t)
+	for _, srcTC := range clusterTestCases() {
+		for _, destTC := range clusterTestCases() {
+			t.Run(fmt.Sprintf("Source=%s/Destination=%s", srcTC.name, destTC.name), func(t *testing.T) {
+				var (
+					mmOpts     = config.MakeMirrorOptions{Prefix: "o_", NoDestPrefix: true, Rev: 4}
+					sourcekvs  = []testutils.KV{{Key: "o_key1", Val: "val1"}, {Key: "o_key2", Val: "val2"}, {Key: "o_key3", Val: "val3"}, {Key: "o_key4", Val: "val4"}}
+					destkvs    = []testutils.KV{{Key: "key3", Val: "val3"}, {Key: "key4", Val: "val4"}}
+					srcprefix  = "o_"
+					destprefix = "key"
+				)
+
+				testMirror(t, srcTC, destTC, mmOpts, sourcekvs, destkvs, srcprefix, destprefix)
+
+			})
+		}
+	}
+}
+
+func testMirror(t *testing.T, srcTC, destTC testCase, mmOpts config.MakeMirrorOptions, sourcekvs, destkvs []testutils.KV, srcprefix, destprefix string) {
 	t.Helper()
 
 	ctx, cancel := context.WithTimeout(t.Context(), 30*time.Second)
@@ -81,95 +139,6 @@ func testMirror(t *testing.T, srcTC, destTC testCase, mmOpts config.MakeMirrorOp
 	require.NoError(t, err)
 
 	// Destination assertion
-	err := destClient.Watch(ctx, destprefix, config.WatchOptions{Prefix: true, Revision: 1})
+	watchChan := destClient.Watch(ctx, destprefix, config.WatchOptions{Prefix: true, Revision: 1})
 
-}
-
-func TestCtlV3MakeMirror(t *testing.T)                 { testCtl(t, makeMirrorTest) }
-func TestCtlV3MakeMirrorModifyDestPrefix(t *testing.T) { testCtl(t, makeMirrorModifyDestPrefixTest) }
-func TestCtlV3MakeMirrorNoDestPrefix(t *testing.T)     { testCtl(t, makeMirrorNoDestPrefixTest) }
-func TestCtlV3MakeMirrorWithWatchRev(t *testing.T)     { testCtl(t, makeMirrorWithWatchRev) }
-
-func makeMirrorTest(cx ctlCtx) {
-	var (
-		flags  []string
-		kvs    = []kv{{"key1", "val1"}, {"key2", "val2"}, {"key3", "val3"}}
-		kvs2   = []kvExec{{key: "key1", val: "val1"}, {key: "key2", val: "val2"}, {key: "key3", val: "val3"}}
-		prefix = "key"
-	)
-	testMirrorCommand(cx, flags, kvs, kvs2, prefix, prefix)
-}
-
-func makeMirrorModifyDestPrefixTest(cx ctlCtx) {
-	var (
-		flags      = []string{"--prefix", "o_", "--dest-prefix", "d_"}
-		kvs        = []kv{{"o_key1", "val1"}, {"o_key2", "val2"}, {"o_key3", "val3"}}
-		kvs2       = []kvExec{{key: "d_key1", val: "val1"}, {key: "d_key2", val: "val2"}, {key: "d_key3", val: "val3"}}
-		srcprefix  = "o_"
-		destprefix = "d_"
-	)
-	testMirrorCommand(cx, flags, kvs, kvs2, srcprefix, destprefix)
-}
-
-func makeMirrorNoDestPrefixTest(cx ctlCtx) {
-	var (
-		flags      = []string{"--prefix", "o_", "--no-dest-prefix"}
-		kvs        = []kv{{"o_key1", "val1"}, {"o_key2", "val2"}, {"o_key3", "val3"}}
-		kvs2       = []kvExec{{key: "key1", val: "val1"}, {key: "key2", val: "val2"}, {key: "key3", val: "val3"}}
-		srcprefix  = "o_"
-		destprefix = "key"
-	)
-
-	testMirrorCommand(cx, flags, kvs, kvs2, srcprefix, destprefix)
-}
-
-func makeMirrorWithWatchRev(cx ctlCtx) {
-	var (
-		flags      = []string{"--prefix", "o_", "--no-dest-prefix", "--rev", "4"}
-		kvs        = []kv{{"o_key1", "val1"}, {"o_key2", "val2"}, {"o_key3", "val3"}, {"o_key4", "val4"}}
-		kvs2       = []kvExec{{key: "key3", val: "val3"}, {key: "key4", val: "val4"}}
-		srcprefix  = "o_"
-		destprefix = "key"
-	)
-
-	testMirrorCommand(cx, flags, kvs, kvs2, srcprefix, destprefix)
-}
-
-func testMirrorCommand(cx ctlCtx, flags []string, sourcekvs []kv, destkvs []kvExec, srcprefix, destprefix string) {
-	// set up another cluster to mirror with
-	mirrorcfg := e2e.NewConfigAutoTLS()
-	mirrorcfg.ClusterSize = 1
-	mirrorcfg.BasePort = 10000
-	mirrorctx := ctlCtx{
-		t:           cx.t,
-		cfg:         *mirrorcfg,
-		dialTimeout: 7 * time.Second,
-	}
-
-	mirrorepc, err := e2e.NewEtcdProcessCluster(context.TODO(), cx.t, e2e.WithConfig(&mirrorctx.cfg))
-	if err != nil {
-		cx.t.Fatalf("could not start etcd process cluster (%v)", err)
-	}
-	mirrorctx.epc = mirrorepc
-
-	defer func() {
-		if err = mirrorctx.epc.Close(); err != nil {
-			cx.t.Fatalf("error closing etcd processes (%v)", err)
-		}
-	}()
-
-	cmdArgs := append(cx.PrefixArgs(), "make-mirror")
-	cmdArgs = append(cmdArgs, flags...)
-	cmdArgs = append(cmdArgs, fmt.Sprintf("localhost:%d", mirrorcfg.BasePort))
-	proc, err := e2e.SpawnCmd(cmdArgs, cx.envMap)
-	require.NoError(cx.t, err)
-	defer func() {
-		require.NoError(cx.t, proc.Stop())
-	}()
-
-	for i := range sourcekvs {
-		require.NoError(cx.t, ctlV3Put(cx, sourcekvs[i].key, sourcekvs[i].val, ""))
-	}
-	require.NoError(cx.t, ctlV3Get(cx, []string{srcprefix, "--prefix"}, sourcekvs...))
-	require.NoError(cx.t, ctlV3Watch(mirrorctx, []string{destprefix, "--rev", "1", "--prefix"}, destkvs...))
 }
